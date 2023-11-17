6d8b6376da963635974365575b0b99f86183656e
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-11-17 01:38:00 -0500

    CRUD estadios completo

---
+++ Controllers/EstadioController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_FUT_NFL.Data;
+using MVC_FUT_NFL.Models;
+
+namespace MVC_FUT_NFL.Controllers
+{
+    public class EstadioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            using (EstadioDAO db = new EstadioDAO())
+            {
+                return View(db.Listar());
+            }
+        }
+        [HttpGet]
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Agregar(Estadio estadio)
+        {
+            using (EstadioDAO db = new EstadioDAO())
+            {
+                db.Agregar(estadio);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            using (EstadioDAO db = new EstadioDAO())
+            {
+                var est = db.Buscar(id);
+                if (est != null)
+                {
+                    return View(est);
+                    // retorna vista prod encontrado
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Editar(Estadio estadio)
+        {
+            using (EstadioDAO db = new EstadioDAO())
+            {
+                db.Editar(estadio);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Eliminar(int id)
+        {
+            using (EstadioDAO db = new EstadioDAO())
+            {
+                var est = db.Buscar(id);
+                if (est != null)
+                {
+                    return View(est);
+                    // retorna vista prod encontrado
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Eliminar(int id, bool flag = false)
+        {
+            using (EstadioDAO db = new EstadioDAO())
+            {
+                db.Eliminar(id);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Detalle(int id)
+        {
+            using (EstadioDAO db = new EstadioDAO())
+            {
+                var estadio = db.Buscar(id);
+                return View(estadio);
+            }
+        }
+    }
+}
---
+++ Data/EstadioDAO.cs
@@ -0,0 +1,66 @@
+﻿using MVC_FUT_NFL.Models;
+
+namespace MVC_FUT_NFL.Data
+{
+    public class EstadioDAO:IDisposable
+    {
+        NflBdContext db = new NflBdContext();
+        private bool disposedValue;
+
+        public int Agregar(Estadio estadio)
+        {
+            db.Estadios.Add(estadio);
+            return db.SaveChanges();
+        }
+        public int Editar(Estadio estadio)
+        {
+            db.Estadios.Update(estadio);
+            return db.SaveChanges();
+        }
+        public int Eliminar(int idestadio)
+        {
+            var query = db.Estadios.Where(e => e.Id == idestadio).SingleOrDefault();
+            db.Estadios.Remove(query);
+            return db.SaveChanges();
+        }
+        public Estadio Buscar(int idestadio)
+        {
+            var query = db.Estadios.Where(e => e.Id == idestadio).SingleOrDefault();
+            return query;
+        }
+        public List<Estadio> Listar()
+        {
+            var query = db.Estadios.Take(100).ToList();
+            return query;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: eliminar el estado administrado (objetos administrados)
+                }
+
+                // TODO: liberar los recursos no administrados (objetos no administrados) y reemplazar el finalizador
+                // TODO: establecer los campos grandes como NULL
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: reemplazar el finalizador solo si "Dispose(bool disposing)" tiene código para liberar los recursos no administrados
+        // ~EstadioDAO()
+        // {
+        //     // No cambie este código. Coloque el código de limpieza en el método "Dispose(bool disposing)".
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // No cambie este código. Coloque el código de limpieza en el método "Dispose(bool disposing)".
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
