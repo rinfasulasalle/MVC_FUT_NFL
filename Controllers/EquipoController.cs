7bbe8df831d9dc99b591b82a422ee6210e8c4f22
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-11-17 01:03:12 -0500

    CRUD equipo completo

---
+++ Controllers/EquipoController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_FUT_NFL.Data;
+using MVC_FUT_NFL.Models;
+
+namespace MVC_FUT_NFL.Controllers
+{
+    public class EquipoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            using (EquipoDAO db = new EquipoDAO())
+            {
+                return View(db.Listar());
+            }
+        }
+        [HttpGet]
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Agregar(Equipo equipo)
+        {
+            using (EquipoDAO db = new EquipoDAO())
+            {
+                db.Agregar(equipo);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            using (EquipoDAO db = new EquipoDAO())
+            {
+                var equi = db.Buscar(id);
+                if (equi != null)
+                {
+                    return View(equi);
+                    // retorna vista prod encontrado
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Editar(Equipo equipo)
+        {
+            using (EquipoDAO db = new EquipoDAO())
+            {
+                db.Editar(equipo);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Eliminar(int id)
+        {
+            using (EquipoDAO db = new EquipoDAO())
+            {
+                var equi = db.Buscar(id);
+                if (equi != null)
+                {
+                    return View(equi);
+                    // retorna vista prod encontrado
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Eliminar(int id, bool flag = false)
+        {
+            using (EquipoDAO db = new EquipoDAO())
+            {
+                db.Eliminar(id);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Detalle(int id)
+        {
+            using (EquipoDAO db = new EquipoDAO())
+            {
+                var equipo = db.Buscar(id);
+                return View(equipo);
+            }
+        }
+    }
+}
---
+++ Data/EquipoDAO.cs
@@ -0,0 +1,66 @@
+﻿using MVC_FUT_NFL.Models;
+
+namespace MVC_FUT_NFL.Data
+{
+    public class EquipoDAO:IDisposable
+    {
+        NflBdContext db = new NflBdContext();
+        private bool disposedValue;
+
+        public int Agregar(Equipo equipo)
+        {
+            db.Equipos.Add(equipo);
+            return db.SaveChanges();
+        }
+        public int Editar(Equipo equipo)
+        {
+            db.Equipos.Update(equipo);
+            return db.SaveChanges();
+        }
+        public int Eliminar(int idequipo)
+        {
+            var query = db.Equipos.Where(e => e.Id == idequipo).SingleOrDefault();
+            db.Equipos.Remove(query);
+            return db.SaveChanges();
+        }
+        public Equipo Buscar(int idequipo)
+        {
+            var query = db.Equipos.Where(e => e.Id == idequipo).SingleOrDefault();
+            return query;
+        }
+        public List<Equipo> Listar()
+        {
+            var query = db.Equipos.Take(100).ToList();
+            return query;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: eliminar el estado administrado (objetos administrados)
+                }
+
+                // TODO: liberar los recursos no administrados (objetos no administrados) y reemplazar el finalizador
+                // TODO: establecer los campos grandes como NULL
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: reemplazar el finalizador solo si "Dispose(bool disposing)" tiene código para liberar los recursos no administrados
+        // ~EquipoDAO()
+        // {
+        //     // No cambie este código. Coloque el código de limpieza en el método "Dispose(bool disposing)".
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // No cambie este código. Coloque el código de limpieza en el método "Dispose(bool disposing)".
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
