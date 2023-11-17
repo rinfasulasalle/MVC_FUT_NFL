1d9bee502e2f0501dead84c57d2d0326ee09dd81
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-11-17 02:22:30 -0500

    CRUD partidos completo

---
+++ Controllers/PartidoController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_FUT_NFL.Data;
+using MVC_FUT_NFL.Models;
+
+namespace MVC_FUT_NFL.Controllers
+{
+    public class PartidoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            using (PartidoDAO db = new PartidoDAO())
+            {
+                return View(db.Listar());
+            }
+        }
+        [HttpGet]
+        public IActionResult Agregar()
+        {
+            CargarListas();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Agregar(Partido partido)
+        {
+            using (PartidoDAO db = new PartidoDAO())
+            {
+                db.Agregar(partido);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            CargarListas();
+            using (PartidoDAO db = new PartidoDAO())
+            {
+                var part = db.Buscar(id);
+                if (part != null)
+                {
+                    return View(part);
+                    // retorna vista part encontrado
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Editar(Partido partido)
+        {
+            using (PartidoDAO db = new PartidoDAO())
+            {
+                db.Editar(partido);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Eliminar(int id)
+        {
+            using (PartidoDAO db = new PartidoDAO())
+            {
+                var parti = db.Buscar(id);
+                if (parti != null)
+                {
+                    return View(parti);
+                    // retorna vista prod encontrado
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Eliminar(int id, bool flag = false)
+        {
+            using (PartidoDAO db = new PartidoDAO())
+            {
+                db.Eliminar(id);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Detalle(int id)
+        {
+            using (PartidoDAO db = new PartidoDAO())
+            {
+                var partido = db.Buscar(id);
+                return View(partido);
+            }
+        }
+        void CargarListas()
+        {
+            using (CatalogoDAO db = new CatalogoDAO())
+            {
+                ViewBag.IdEstadio = db.ListarEstadios();
+                ViewBag.IdEquipo = db.ListarEquipos();
+
+            }
+        }
+    }
+}
---
+++ Data/PartidoDAO.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_FUT_NFL.Models;
+namespace MVC_FUT_NFL.Data
+{
+    public class PartidoDAO:IDisposable
+    {
+        NflBdContext db = new NflBdContext();
+        private bool disposedValue;
+
+        public int Agregar(Partido partido)
+        {
+            db.Partidos.Add(partido);
+            return db.SaveChanges();
+        }
+        public int Editar(Partido partido)
+        {
+            db.Partidos.Update(partido);
+            return db.SaveChanges();
+        }
+        public int Eliminar(int idpartido)
+        {
+            var query = db.Partidos.Where(p => p.Id == idpartido).SingleOrDefault();
+            db.Partidos.Remove(query);
+            return db.SaveChanges();
+        }
+        public Partido Buscar(int idpartido)
+        {
+            Listar(); // para que muestre en detalel
+            var query = db.Partidos.Where(p => p.Id == idpartido).SingleOrDefault();
+            return query;
+        }
+        public List<Partido> Listar()
+        {
+            // Incluye las propiedades de navegación
+            var query = db.Partidos
+                .Include(p => p.IdEquipo1Navigation)
+                .Include(p => p.IdEquipo2Navigation)
+                .Include(p => p.IdEstadioNavigation)
+                .Take(100)
+                .ToList();
+
+            return query;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: eliminar el estado administrado (objetos administrados)
+                }
+
+                // TODO: liberar los recursos no administrados (objetos no administrados) y reemplazar el finalizador
+                // TODO: establecer los campos grandes como NULL
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: reemplazar el finalizador solo si "Dispose(bool disposing)" tiene código para liberar los recursos no administrados
+        // ~PartidoDAO()
+        // {
+        //     // No cambie este código. Coloque el código de limpieza en el método "Dispose(bool disposing)".
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // No cambie este código. Coloque el código de limpieza en el método "Dispose(bool disposing)".
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
