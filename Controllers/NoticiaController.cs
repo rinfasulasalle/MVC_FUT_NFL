9c6b8acaa20fbaf334261c3c01c9ad9ad01f5854
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-11-20 14:02:36 -0500

    CRUD noticias completo + admin

---
+++ Controllers/NoticiaController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_FUT_NFL.Data;
+using MVC_FUT_NFL.Models;
+
+namespace MVC_FUT_NFL.Controllers
+{
+    public class NoticiaController : Controller
+    {
+        // INDEX
+        [HttpGet]
+        public IActionResult Index()
+        {
+            using (NoticiaDAO db = new NoticiaDAO())
+            {
+                return View(db.Listar());
+            }
+        }
+        // CREATE
+        [HttpGet]
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Agregar(Noticia noticia)
+        {
+            using (NoticiaDAO db = new NoticiaDAO())
+            {
+                db.Agregar(noticia);
+            }
+            return RedirectToAction("Index");
+        }
+        // UPDATE
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            using (NoticiaDAO db = new NoticiaDAO())
+            {
+                var noti = db.Buscar(id);
+                if (noti != null)
+                {
+                    return View(noti);
+                    // retorna vista prod encontrado
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Editar(Noticia noticia)
+        {
+            using (NoticiaDAO db = new NoticiaDAO())
+            {
+                db.Editar(noticia);
+            }
+            return RedirectToAction("Index");
+        }
+        // DELETE
+        [HttpGet]
+        public IActionResult Eliminar(int id)
+        {
+            using (NoticiaDAO db = new NoticiaDAO())
+            {
+                var juga = db.Buscar(id);
+                if (juga != null)
+                {
+                    return View(juga);
+                    // retorna vista prod encontrado
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Eliminar(int id, bool flag = false)
+        {
+            using (NoticiaDAO db = new NoticiaDAO())
+            {
+                db.Eliminar(id);
+            }
+            return RedirectToAction("Index");
+        }
+        // READ
+        [HttpGet]
+        public IActionResult Detalle(int id)
+        {
+            using (NoticiaDAO db = new NoticiaDAO())
+            {
+                var noti = db.Buscar(id);
+                return View(noti);
+            }
+        }
+        [HttpGet]
+        public IActionResult Admin()
+        {
+            using (NoticiaDAO db = new NoticiaDAO())
+            {
+                return View(db.Listar());
+            }
+        }
+    }
+}
---
+++ Data/NoticiaDAO.cs
@@ -0,0 +1,66 @@
+﻿using MVC_FUT_NFL.Models;
+
+namespace MVC_FUT_NFL.Data
+{
+    public class NoticiaDAO:IDisposable
+    {
+        NflBdContext db = new NflBdContext();
+        private bool disposedValue;
+
+        public int Agregar(Noticia noticia)
+        {
+            db.Noticias.Add(noticia);
+            return db.SaveChanges();
+        }
+        public int Editar(Noticia noticia)
+        {
+            db.Noticias.Update(noticia);
+            return db.SaveChanges();
+        }
+        public int Eliminar(int idnoticia)
+        {
+            var query = db.Noticias.Where(e => e.Id == idnoticia).SingleOrDefault();
+            db.Noticias.Remove(query);
+            return db.SaveChanges();
+        }
+        public Noticia Buscar(int idnoticia)
+        {
+            var query = db.Noticias.Where(e => e.Id == idnoticia).SingleOrDefault();
+            return query;
+        }
+        public List<Noticia> Listar()
+        {
+            var query = db.Noticias.ToList();
+            return query;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: eliminar el estado administrado (objetos administrados)
+                }
+
+                // TODO: liberar los recursos no administrados (objetos no administrados) y reemplazar el finalizador
+                // TODO: establecer los campos grandes como NULL
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: reemplazar el finalizador solo si "Dispose(bool disposing)" tiene código para liberar los recursos no administrados
+        // ~NoticiaDAO()
+        // {
+        //     // No cambie este código. Coloque el código de limpieza en el método "Dispose(bool disposing)".
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // No cambie este código. Coloque el código de limpieza en el método "Dispose(bool disposing)".
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
