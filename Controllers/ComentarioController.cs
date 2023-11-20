4d07a9ecb57cf31e7d2910d084877355dd3e9b56
rinfasulasalle <rinfas@ulasalle.edu.pe>
2023-11-20 12:05:57 -0500

    CRUD comentarios completo

---
+++ Controllers/ComentarioController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_FUT_NFL.Data;
+using MVC_FUT_NFL.Models;
+
+namespace MVC_FUT_NFL.Controllers
+{
+    public class ComentarioController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            using (ComentarioDAO db = new ComentarioDAO())
+            {
+                return View(db.Listar());
+            }
+        }
+        [HttpGet]
+        public IActionResult Agregar()
+        {
+            CargarListas();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Agregar(Comentario comentario)
+        {
+            using (ComentarioDAO db = new ComentarioDAO())
+            {
+                db.Agregar(comentario);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            CargarListas();
+            using (ComentarioDAO db = new ComentarioDAO())
+            {
+                var com = db.Buscar(id);
+                if (com != null)
+                {
+                    return View(com);
+                    // retorna vista part encontrado
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Editar(Comentario comentario)
+        {
+            using (ComentarioDAO db = new ComentarioDAO())
+            {
+                db.Editar(comentario);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Eliminar(int id)
+        {
+            using (ComentarioDAO db = new ComentarioDAO())
+            {
+                var com = db.Buscar(id);
+                if (com != null)
+                {
+                    return View(com);
+                    // retorna vista prod encontrado
+                }
+            }
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Eliminar(int id, bool flag = false)
+        {
+            using (ComentarioDAO db = new ComentarioDAO())
+            {
+                db.Eliminar(id);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Detalle(int id)
+        {
+            using (ComentarioDAO db = new ComentarioDAO())
+            {
+                var comentario = db.Buscar(id);
+                return View(comentario);
+            }
+        }
+
+        void CargarListas()
+        {
+            using (CatalogoDAO db = new CatalogoDAO())
+            {
+                ViewBag.IdPartido = db.ListarPartidos();
+            }
+        }
+    }
+}
---
+++ Data/ComentarioDAO.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_FUT_NFL.Models;
+
+namespace MVC_FUT_NFL.Data
+{
+    public class ComentarioDAO:IDisposable
+    {
+        NflBdContext db = new NflBdContext();
+        private bool disposedValue;
+
+        public int Agregar(Comentario comentario)
+        {
+            db.Comentarios.Add(comentario);
+            return db.SaveChanges();
+        }
+        public int Editar(Comentario comentario)
+        {
+            db.Comentarios.Update(comentario);
+            return db.SaveChanges();
+        }
+        public int Eliminar(int idcomentario)
+        {
+            var query = db.Comentarios.Where(c => c.Id == idcomentario).SingleOrDefault();
+            db.Comentarios.Remove(query);
+            return db.SaveChanges();
+        }
+        public Comentario Buscar(int idcomentario)
+        {
+            Listar(); // para que muestre en detalel
+            var query = db.Comentarios.Where(c => c.Id == idcomentario).SingleOrDefault();
+            return query;
+        }
+        public List<Comentario> Listar()
+        {
+            var query = db.Comentarios
+                .Include(r => r.IdPartidoNavigation)
+                .ToList();
+            return query;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: eliminar el estado administrado (objetos administrados)
+                }
+
+                // TODO: liberar los recursos no administrados (objetos no administrados) y reemplazar el finalizador
+                // TODO: establecer los campos grandes como NULL
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: reemplazar el finalizador solo si "Dispose(bool disposing)" tiene código para liberar los recursos no administrados
+        // ~ComentarioDAO()
+        // {
+        //     // No cambie este código. Coloque el código de limpieza en el método "Dispose(bool disposing)".
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // No cambie este código. Coloque el código de limpieza en el método "Dispose(bool disposing)".
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
